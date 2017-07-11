e65232181af2af9cf2a00949c958699d1483e7ae
Sajjan Heerah <sajjan.heerah@mail.utoronto.ca>
2017-07-11 08:46:16 -0700

    Updated gps script so writes a new file each run and alterred write data format slightly

--- gps_read.py
+++ gps_read.py
@@ -21,42 +21,46 @@ now = dt.datetime.now().strftime("%Y%m%d")
 base_name = now + "_" + "GPS_"
 file_number = 0
 for file in os.listdir():
-    if file == base_name:
+    print(file)
+    if file == base_name + str(file_number).zfill(2):
         file_number = file_number + 1
 file_number = str(file_number).zfill(2)
 
 header = "GPSUTCDate, GPSUTCTime, Lat, Long, masl, CompTime\n"
 log_file = open("./" + base_name + file_number, mode="w")
+print(log_file)
 log_file.write(header)
 
 
 def gps_read():
-    gps_ser = serial.Serial("COM6", baudrate=4800, timeout=1)
+    gps_ser = serial.Serial("COM4", baudrate=4800, timeout=1)
 #    data_dict = {"GPGGA": [], "GPRMC": [], "GPGSA": [], "GPGSV": []}
     while True:
         gps = str(gps_ser.readline())[2:-5].split(",")
 #        data_dict[sentence[0]].append(sentence[1:])
-
         if gps[0] == "$GPGGA":
-            time = gps[1]
             lat = str(gps[2])
+            time = float(gps[1])
             try:
                 lat = float(lat[0:2]) + float(lat[2:])/60.
                 lon = str(gps[4])
                 lon = -float(lon[0:3]) - float(lon[3:])/60.
                 alt = float(gps[9])
+            except KeyboardInterrupt:
+                log_file.close()
+                raise
             except ValueError:
                 time, lat, lon, alt = (np.nan, np.nan, np.nan, np.nan)
-            data_str = [time, lat, lon, alt, str(dt.datetime.now())]
-            write_str = date + ", " + str(data_str)[1:-1] + "\n"
+            data_str = [time, lat, lon, alt]
+            write_str = date + ", " + str(data_str)[1:-1] + ", " + str(dt.datetime.now()) + "\n"
+            print(write_str)
             log_file.write(write_str)
             log_file.flush()
     log_file.close()
 
 
 def main():
-    t1 = threading.Thread(target=gps_read)
-    t1.start()
+    gps_read()
 
 if __name__ == "__main__":
     main()
